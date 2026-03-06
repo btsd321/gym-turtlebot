88bfe20c153cb7a09b4c7ec02cb86cfce50cc076
btsd321 <1123347532@qq.com>
2026-03-06 23:15:39 +0800

    feat: 更新 devcontainer.json，移除 LD_LIBRARY_PATH 设置；更新 simulation.launch.py，默认启用无头模式

--- src/turtlebot4/tb4_gz_sim/launch/simulation.launch.py
+++ src/turtlebot4/tb4_gz_sim/launch/simulation.launch.py
@@ -93,7 +93,7 @@ def generate_launch_description():
 
     declare_headless_cmd = DeclareLaunchArgument(
         'headless',
-        default_value='False',
+        default_value='True',
         description='Run the simulation in headless mode.',
         choices=['True', 'False'],
     )
